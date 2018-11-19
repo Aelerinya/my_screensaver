6da894593cd7c448a51b52871f3f6fb161f892cb
Lucie Philippon (Julien) <julien.philippon@epitech.eu>
2018-11-19 23:47:20 +0100

    Add linked_list structure

---
+++ include/linked_list_t.h
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2018
+** my_screensaver
+** File description:
+** linked_list_t definition
+*/
+
+typedef struct linked_list {
+void *data;
+struct linked_list *next;
+} linked_list_t;
