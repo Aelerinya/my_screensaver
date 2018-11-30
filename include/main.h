67797feedfa12dc6ce6ae5d7f149f07af2bb66c1
Lucie Philippon (Julien) <julien.philippon@epitech.eu>
2018-11-30 16:53:58 +0100

    Update lines() screensaver
    lines() now prints lines only between two lines already printed,
      dividing the screen into variable areas.

--- include/main.h
+++ include/main.h
@@ -12,7 +12,7 @@ const screensaver_t screensaver_list[7] = {
 {"colorfull noise", &colorfull_noise},
 {"bouncing ball changing color", &bouncing},
 {"print random colored squares", &squares},
-{"print horizontal and vertical lines at random", &lines},
+{"print lines between other lines", &lines},
 {"displays a raw image file", &display_image},
 {"snake game (Controls : ZQSD)", &snake},
 {"phantomatic shadows", &phantoms}
--- src/screensavers/lines.c
+++ src/screensavers/lines.c
@@ -9,12 +9,33 @@
 #include <stdlib.h>
 #include "prototypes.h"
 
+static void draw_line(ssv_data_t *data, int dir, unsigned int pos)
+{
+    int counter = 0;
+    unsigned int limit = (dir) ? data->fb->height : data->fb->width;
+    int part;
+
+    for (unsigned int i = 0; i < limit; i++)
+        if (get_pixel(data->fb, (dir) ? pos : i, (dir) ? i : pos)->r != 0)
+            counter++;
+    part = rand() % (counter + 1);
+    counter = 0;
+    for (unsigned int i = 0; i < limit; i++) {
+        if (get_pixel(data->fb, (dir) ? pos : i, (dir) ? i : pos)->r != 0)
+            counter++;
+        if (counter == part)
+            put_pixel(data->fb, (dir) ? pos : i, (dir) ? i : pos, sfWhite);
+        if (counter > part)
+            break;
+    }
+}
+
 int lines(ssv_data_t *data)
 {
     unsigned int pos;
     int dir;
     sfClock *clock = sfClock_create();
-    sfInt64 interval = sfSeconds(1).microseconds;
+    sfInt64 interval = sfMilliseconds(500).microseconds;
     sfInt64 current;
 
     reset_framebuffer(data->fb, sfBlack);
@@ -23,8 +44,7 @@ int lines(ssv_data_t *data)
         if (current > interval) {
             dir = rand() % 2;
             pos = rand() % ((dir) ? data->fb->width : data->fb->height);
-            for (unsigned int i = 0; i < ((dir) ? data->fb->height : data->fb->width); i++)
-                put_pixel(data->fb, (dir) ? pos : i, (dir) ? i : pos, sfWhite);
+            draw_line(data, dir, pos);
             sfClock_restart(clock);
         }
         display_framebuffer(data);
